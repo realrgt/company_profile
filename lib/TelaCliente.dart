e7b0d5a1931132a1374819a1452dedf77cb29d0b
realrgt <ergitorubildo@gmail.com>
2020-02-28 09:23:27 +0200

    Tela contato

--- lib/TelaCliente.dart
+++ lib/TelaCliente.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TelaCliente extends StatefulWidget {
   @override
@@ -8,6 +8,37 @@ class TelaCliente extends StatefulWidget {
 class _TelaClienteState extends State<TelaCliente> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clientes"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 25),
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("images/detalhe_cliente.png"),
+                    Text("Clientes")
+                  ],
+                ),
+              ),
+              Image.asset("images/cliente1.png"),
+              Padding(
+                padding: EdgeInsets.only(bottom: 15.0),
+                child: Text("Empresa de Software"),
+              ),
+              Image.asset("images/cliente2.png"),
+              Text("Empresa de Consultoria")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/TelaContato.dart
+++ lib/TelaContato.dart
@@ -1,5 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TelaContato extends StatefulWidget {
   @override
@@ -9,6 +9,40 @@ class TelaContato extends StatefulWidget {
 class _TelaContatoState extends State<TelaContato> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clientes"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 15),
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("images/detalhe_cliente.png"),
+                    Text("Nossos Serviços")
+                  ],
+                ),
+              ),
+              Text(
+                  "Consultoria",
+                  textAlign: TextAlign.justify
+              ),Text(
+                  "Preços",
+                  textAlign: TextAlign.justify
+              ),Text(
+                  "Acompanhamento de Projetos",
+                  textAlign: TextAlign.justify
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
